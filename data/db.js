98f8f0807fdb7522db3546db041abe84abfb3aac
Chris Gately <gately.c@husky.neu.edu>
2019-04-12 22:01:25 -0400

    Added answer api

--- data/db.js
+++ data/db.js
@@ -1,8 +1,8 @@
 module.exports = function () {
     const mongoose = require('mongoose');
     const databaseName = 'white-board';
-    //var connectionString = 'mongodb://localhost/';
-    var connectionString = 'mongodb://heroku_d8lrztq8:2331v3fev4vpbp8jurja4gdcu@ds139576.mlab.com:39576/heroku_d8lrztq8'
+    var connectionString = 'mongodb://localhost/';
+    //var connectionString = 'mongodb://heroku_d8lrztq8:2331v3fev4vpbp8jurja4gdcu@ds139576.mlab.com:39576/heroku_d8lrztq8'
     connectionString += databaseName;
     mongoose.connect(connectionString)
 }
