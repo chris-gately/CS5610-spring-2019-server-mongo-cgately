7c5efb2a9885cc7cfbc546278cf9c9b2b898f305
Chris Gately <gately.c@husky.neu.edu>
2019-04-12 23:48:24 -0400

    Modified mongoose.connect

--- data/db.js
+++ data/db.js
@@ -4,5 +4,5 @@ module.exports = function () {
     //var connectionString = 'mongodb://localhost/';
     var connectionString = 'mongodb://heroku_d8lrztq8:2331v3fev4vpbp8jurja4gdcu@ds139576.mlab.com:39576/heroku_d8lrztq8/'
     //connectionString += databaseName;
-    mongoose.connect(connectionString)
+    mongoose.connect(process.env.MONGODB_URI)
 }
